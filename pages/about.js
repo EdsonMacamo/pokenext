e6053dc23fb996df5608d98588e5fb9351ef9ac7
Edson Macamo <edsonmacamo85@gmail.com>
2022-04-16 16:03:04 +0200

    Comiit do projecto

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from '../styles/About.module.css'
+export default function About(){
+  return(
+      <div className={styles.about}>
+          <h1>Sobre o Projecto</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
+          <Image src="/images/charizard.png" width="300" height="300" alt="Chairzard" />
+      </div>
+  )
+}
